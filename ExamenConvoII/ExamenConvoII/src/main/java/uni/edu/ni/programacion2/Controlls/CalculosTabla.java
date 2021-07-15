a7378425bac841e2cece602bfe8baa3bd3bd42da
hector2117 <70295230+hector2117@users.noreply.github.com>
2021-07-15 11:03:01 -0600

    Add files via upload

---
+++ ExamenConvoII/ExamenConvoII/src/main/java/uni/edu/ni/programacion2/Controlls/CalculosTabla.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uni.edu.ni.programacion2.Controlls;
+
+/**
+ *
+ * @author Sistemas-11
+ */
+public class CalculosTabla {
+    
+}
